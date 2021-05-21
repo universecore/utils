87f906621076ef4dd997b96f3a72fd5a920044cf
Zhanat <panzu87@gmail.com>
2021-05-21 09:40:14 +0600

    new func containsInt

--- utils.go
+++ utils.go
@@ -1,10 +1,18 @@
 package utils
 
 func Contains(a []string, x string) bool {
-  for _, n := range a {
-     if x == n {
-        return true
-     }
-  }
-  return false
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
 }
